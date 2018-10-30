d5cfb79eb266f1b9d23258b271c6298d8968c6bf
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-30 19:59:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solution in this file.
+
+const drivers = [];
+
+// function  lowerCaseDrivers() {return drivers.map(change);} 
+
+ function lowerCaseDrivers  (drivers)  {
+   return drivers.map(driver => driver.toLowerCase()); 
+   }
+   
+// let nameArr = ['jessy jones', 'mike jones']; 
+ 
+// let splitArr = nameArr[i].split(" ");
+ 
+// let names = {}; 
+ 
+// names.firstName = splitArr[0] ;
+ 
+// names.lastName = splitArr[1] ; 
+ 
+// for (word of nameArr) {word.split(" ")}
+ 
+// drivers.map(driver => driver.split(" ") )
+ 
+// drivers.map(driver => newObj.firstName = driver[0]; newObj.lastName = driver[1])
+ 
+ 
+ 
+ 
+ 
+ function nameToAttributes (drivers) {
+   let newArr2 =  drivers.map(d => {
+     let newArr = d.split(" ");
+     return {firstName: newArr[0], lastName: newArr[1]};
+   })
+   return newArr2 
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
